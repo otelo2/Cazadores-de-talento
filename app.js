79163787bbda70b92fb4feb7511c8caf1dd3f5eb
Otelo2 <josean2009@live.com.mx>
2021-11-19 08:50:12 -0600

    crear cita admin

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.post('/cita/crear_cita_cazador.html', function(request, response) {
   response.sendFile(__dirname + '/cita/crear_cita_cazador.html');
 });
 
+app.post('/cita/crear_cita_administrador.html', function(request, response) {
+
+  clases.crearCitaA(request.body.lista_de_cazadores, request.body.lista_de_talentos, request.body.horario, request.body.lugar)
+  response.sendFile(__dirname + '/cita/crear_cita_administrador.html');
+});
+
 
 //Puerto de nuestro servidor
 app.listen(8080, function() {
